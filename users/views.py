679bbb181e78add32b41a2e972fe9b4135c44fd9
[Gaia <[dzhayadzoy@gmail.com]>
2026-03-06 15:05:41 +0500

    edited def activate()

--- users/views.py
+++ users/views.py
@@ -64,21 +64,26 @@ swe.set_ephe_path('/home/gaia/Документы/eph files')
 
 
 def activate(request, uidb64, token):
+
     User = get_user_model()
+
     try:
         uid = force_str(urlsafe_base64_decode(uidb64))
         user = User.objects.get(pk=uid)
-    except:
+    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
         user = None
 
     if user is not None and account_activation_token.check_token(user, token):
         user.is_active = True
         user.save()
 
-        messages.success(request, "Thank you for your email confirmation. Now you can login your account.")
+        (messages.success
+         (request, "E-mail's been confirmed. Please, log in."))
         return redirect('login')
     else:
         messages.error(request, "Activation link is invalid!")
+        return None
+
 
 def activate_email(request, user, to_email):
     mail_subject = 'Activate your user account.'
